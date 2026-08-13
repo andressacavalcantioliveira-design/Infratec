245029fcb9d022f01c81a566e3002a6534bbed52
andressacavalcantioliveira-design <andressa.cavalcanti.oliveira@escola.pr.gov.br>
2026-08-12 21:18:55 -0300

    Add files via upload

---
+++ dml_tabelas_FK_composta.sql
@@ -0,0 +1,14 @@
+INSERT INTO devolucoes
+(id_entrega, id_funcionario, id_epi, data_devolucao, quantidade, estado)
+VALUES
+(1, 1, 1, '2026-08-10', 1, 'Bom');
+
+INSERT INTO devolucoes
+(id_entrega, id_funcionario, id_epi, data_devolucao, quantidade, estado)
+VALUES
+(2, 2, 2, '2026-08-11', 1, 'Danificado');
+
+INSERT INTO devolucoes
+(id_entrega, id_funcionario, id_epi, data_devolucao, quantidade, estado)
+VALUES
+(3, 3, 3, '2026-08-12', 1, 'Bom');
